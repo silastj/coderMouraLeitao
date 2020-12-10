61601104424eab1514629d4d15650a548673a407
aSilas <silastj@hotmail.com>
2020-12-10 00:14:44 -0300

    Iniciando os exercicios

---
+++ Exercicios/exercicios.js
@@ -0,0 +1,16 @@
+//exercicio 01
+// const nome = document.querySelector('input').value
+// const clique = document.querySelector('button')
+// clique.addEventListener('click', cumprimentar)
+// function cumprimentar(){
+//     return console.log(`Olá ${nome}`)
+// }
+
+//exercicio02
+// const btn = document.querySelector('button')
+// const dias = 173
+// function tranformaEMdias(){
+//     const input = document.querySelector('input').value
+//     return console.log('Quantos dias vc tem de idade, fora o final de semana => ' + input * dias + ' dias')
+// }
+// btn.addEventListener('click', tranformaEMdias)
