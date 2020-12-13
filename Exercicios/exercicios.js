94b4e87f2767ec29b2a6274212e6257a78f85fb8
aSilas <silastj@hotmail.com>
2020-12-12 23:51:11 -0300

    Exercicios em execução

--- Exercicios/exercicios.js
+++ Exercicios/exercicios.js
@@ -14,3 +14,72 @@
 //     return console.log('Quantos dias vc tem de idade, fora o final de semana => ' + input * dias + ' dias')
 // }
 // btn.addEventListener('click', tranformaEMdias)
+
+//exercicio03
+// function calcularSalario(horas = 8, salarioHora = 35){
+//     const totalSalario = horas * salarioHora
+//     return console.log(`O Salario hora é igual: ${salarioHora} e o total do mês é ${totalSalario}`)
+// }
+// calcularSalario()
+
+//exercicio04
+// const p = document.querySelector('p span')
+// const btn = document.querySelector('button')
+// const input = document.querySelector('input')
+// btn.addEventListener('click', MesAnual)
+// function MesAnual(){
+    
+//     const mes = document.querySelector('input').value
+//     switch(parseInt(mes)){
+//         case 1:
+//             p.innerHTML = 'Janeiro'
+//         break;
+//         case 2:
+//             p.innerHTML = 'Fevereiro'
+//         break;
+//         case 3:
+//             p.innerHTML = 'Março'
+//         break;
+//         case 4:
+//             p.innerHTML = 'Abril'
+//         break;
+//         case 5:
+//             p.innerHTML = 'Maio'
+//         break;
+//         case 6:
+//             p.innerHTML = 'Junho'
+//         break;
+//         case 7:
+//             p.innerHTML = 'Julho'
+//         break;
+//         case 8:
+//             p.innerHTML = 'Agosto'
+//         break;
+//         case 9:
+//             p.innerHTML = 'Setembro'
+//         break;
+//         case 10:
+//             p.innerHTML = 'Outubro'
+//         break;
+//         case 11:
+//             p.innerHTML = 'Novembro'
+//         break; 
+//         case 12:
+//             p.innerHTML = 'Dezembro'
+//         break;   
+//         default:
+//             alert(`Desculpe, vc digitou o número ${mes}`)
+//     } input.focus();
+//       input.value = '';
+// }
+
+//exercicio 05
+// function diferencia(a,b){
+//     if(a > b){
+//         return true
+//     } return false
+// }
+// console.log(diferencia(10, 10))
+
+// exercicio 06
+
